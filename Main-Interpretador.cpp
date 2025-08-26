327bd8ef19af3a83bd8271bf2b85ff9a9b0da986
GuilhermeHorta22 <ghorta800@gmail.com>
2025-08-26 16:57:52 -0300

    pilhas de variaveis

--- Main-Interpretador.cpp
+++ Main-Interpretador.cpp
@@ -33,7 +33,116 @@ struct TpVariavel
 	Programa *ponteiro;
 	struct TpVariavel *prox;
 };
-typedef struct TpVariavel variavel;
+typedef struct TpVariavel Variavel;
+
+// -- TAD PILHA VARIAVEL --
+
+//função que inicializa a pilha da variavel
+void initPV(Variavel **pilhaVar)
+{
+	*pilhaVar = NULL;
+}
+
+//função que verifica se a pilha de variavel é nula ou vazia
+char isEmptyPV(Variavel *pilhaVar)
+{
+	return pilhaVar == NULL;
+}
+
+//função que insere a variavel dentro da nossa pilha de variavel
+void pushPV(Variavel **pilhaVar, char *valor, char *identificador, Programa *programa)
+{
+	Variavel *nova = (Variavel*)malloc(sizeof(Variavel));
+	strcpy(nova->valor, valor);
+	strcpy(nova->identificador, identificador);
+	nova->ponteiro = programa;
+	
+	nova->prox = *pilhaVar;
+	*pilhaVar = nova;
+}
+
+//função que vai retirar uma variavel da nossa pilha de variavel
+void popPV(Variavel **pv, Variavel **pvAux)
+{
+	if(!isEmptyPV(*pv))
+	{
+		*pvAux = (Variavel*)malloc(sizeof(Variavel));
+		Variavel *aux; //vai ser usado para deletar a caixinha
+		
+		//fazendo o auxiliar que vem por parametro receber as informações
+		strcpy((*pvAux)->valor, (*pv)->valor);
+		strcpy((*pvAux)->identificador, (*pv)->identificador);
+		(*pvAux)->ponteiro = (*pv)->ponteiro;
+		(*pvAux)->prox = NULL;
+		
+		aux = *pv;
+		*pv = (*pv)->prox;
+		free(aux);
+	}
+}
+
+//função que vai tirar uma variavel da pilha pelo identificador (indepentemente se é topo ou não)
+void popIdentificadorPV(Variavel **pv, char *identificador)
+{
+	Variavel *atual = *pv, *ant = NULL;
+	
+	while(atual != NULL)
+	{
+		if(strcmp(atual->identificador, identificador) == 0)
+		{
+			if(ant == NULL) //1º caso
+				*pv = atual->prox;
+			else
+				ant->prox = atual->prox;
+			
+			free(atual);
+			atual = NULL; //para encerrar a repetição
+		}
+		else
+		{
+			ant = atual;
+			atual = atual->prox;
+		}
+	}
+}
+
+//função que vai retornar o topo da nossa pilha de variavel
+void topPV(Variavel *pv, Variavel **pvAux)
+{
+	*pvAux = pv;
+}
+
+//função que busca uma variavel na pilha de variavel pelo identificador
+Variavel *buscarIdentPV(Variavel *pv, char *identificador)
+{
+	while(pv != NULL && strcmp(pv->identificador, identificador) != 0)
+		pv = pv->prox;
+		
+	if(pv != NULL)
+		return pv;
+	return NULL; //não achou
+}
+
+//função que busca um token dentro da lista
+Programa *buscarToken(Programa *programa, char *tokenBusca)
+{
+	Token *token;
+	
+	while(programa != NULL)
+	{
+		token = programa->token;
+		while(token != NULL)
+		{
+			if(strcmp(token->info, tokenBusca) == 0)
+				return programa; // lista do programa auq tem o token dentro
+			token = token->prox;
+		}
+		programa = programa->prox;
+	}
+	return NULL; //não achou o token
+}
+
+
 
 //função que cria a caixa do nosso token
 Token *CaixaToken(char *info) 
@@ -214,37 +323,43 @@ void ExibirPrograma(Programa *programa) {
 
 //TENHO QUE TERMINAR ESSA FUNÇÕES PARA EXIBIR LINHA A LINHA DO PROGRAMA
 //função que exibe o codigo linha a linha conforme for dando enter
-void exibirExecucao(Programa *programa)
-{
-	Token *linha;
-	
-	while(pProg != NULL)
-	{
-		linha = programa->token;
-		while(linha != NULL)
-		{
-			printf("%s",linha->token);
-			linha=linha->prox;
-		}
-		printf("\n");
-		pProg = pProg->prox;
-	}
-}
+//void exibirExecucao(Programa *programa)
+//{
+//	Token *linha;
+//	
+//	while(pProg != NULL)
+//	{
+//		linha = programa->token;
+//		while(linha != NULL)
+//		{
+//			printf("%s",linha->token);
+//			linha=linha->prox;
+//		}
+//		printf("\n");
+//		pProg = pProg->prox;
+//	}
+//}
 
-//função que exibe o codigo linha a linha conforme for dando enter
-void mostrarLinha(Programa *programa)
-{
-	Token *token = programa->token;
-	
-	while(token != NULL)
-	{
-		printf("%s",token->token);
-		token = token->prox;
-	}
-	
-	printf("\n");
-	Mostrar_Excucao(atual->prox);
-}
+////função que exibe o codigo linha a linha conforme for dando enter
+//void mostrarLinha(Programa *programa)
+//{
+//	Token *token = programa->token;
+//	
+//	while(token != NULL)
+//	{
+//		printf("%s",token->token);
+//		token = token->prox;
+//	}
+//	
+//	printf("\n");
+//	Mostrar_Excucao(atual->prox);
+//}
+
+//função que simula a execução do nosso programa (FALTA FINALIZAR)
+//void simulaExecucao(Programa *programa, Variavel **pv)
+//{
+//	
+//}
 
 //função que contem o menu do nosso simulador de execução
 char menu()
@@ -260,6 +375,9 @@ int main()
 {
 	//lista que vai apontar para a lista de tokens
     Programa *l = NULL; 
+    
+    //lista do tipo pilha que vai guardar todas as informações das nossas variaveis
+    Variavel *pilhaVar = NULL;
     
     char op, nomeArquivo[50];
 	int result;
@@ -283,6 +401,7 @@ int main()
 			
 			case 66: //F8
 				//aqui eu vou chamar a execução do programa linha a linha
+				//simulaExecucao(l, &pilhaVar);
 				
 				break;
 				
